9c9afd60446296c6e21c73df28d51fedf4e29061
Mathieu Ponnou <mathieu.ponnou@gmail.com>
2026-03-18 19:44:02 +0100

    supression de commentaires

--- airflow/dags/tasks/classify_extract_task.py
+++ airflow/dags/tasks/classify_extract_task.py
@@ -37,24 +37,3 @@ def run_classify_and_extract(**context):
         json.dump(result_dict, f, ensure_ascii=False, indent=2)
 
     return result_dict
-
-
-"""
-def run_classify_and_extract(**context):
-    ti = context["ti"]
-
-    text = ti.xcom_pull(task_ids="ocr_task")
-
-    # TODO: remplacer par la vraie classification + extraction (Personne 3)
-    return {
-        "document_type": "facture",
-        "champs": {
-            "supplier_name": "Mock Supplier",
-            "siret": "12345678901234",
-            "tva": "FR12345678901",
-            "date_emission": "2024-01-01",
-            "montant_ht": 100.0,
-            "montant_ttc": 120.0,
-        }
-    }
-"""
